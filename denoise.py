91e89101d9192c347e5ef6caf3422d64ea4d5b93
sieunhanbom04 <31831447+sieunhanbom04@users.noreply.github.com>
2019-12-24 11:28:51 +0100

    Add files via upload

--- denoise.py
+++ denoise.py
@@ -6,26 +6,27 @@ from SinGAN.imresize import imresize_to_shape
 import SinGAN.functions as functions
 from matplotlib import pyplot as plt
 from skimage import io as img
-import bm3d
+import pybm3d
+from skimage.measure import compare_psnr
 from skimage.restoration import (denoise_tv_chambolle, denoise_bilateral, denoise_wavelet, estimate_sigma)
-
-def PSNR(img1, img2, peak=1):
-    '''
-    Computes the PSNR 'metric' between two images assumed to be in the range [0,1]
-    '''
-    import numpy as np
-
-    x = ((np.array(img1).squeeze() - np.array(img2).squeeze()).flatten() )
-    return (10*np.log10(peak**2 / np.mean(x**2)))
-
+"""
+def noiser_and_ref(clean_img, opt):
+    noise = np.random.normal(scale = opt.noise, size = clean_img.shape).astype(np.int16)
+    noisy_img = (clean_img.astype(np.int16) + noise).clip(0,255).astype(np.uint8)
+    ref_img = pybm3d.bm3d.bm3d(noisy_img, opt.noise)
+    ref_img_torch = functions.np2torch(ref_img.astype(np.float), opt)
+    ref_img_torch = ref_img_torch[:,0:3,:,:]
+    return noisy_img, ref_img, ref_img_torch
+"""
 
 if __name__ == '__main__':
     parser = get_arguments()
     parser.add_argument('--input_dir', help = 'input image dir', default = 'Input/Images')
+    parser.add_argument('--noise_dir', help = 'folder containing noisy image and bm3d image', default = 'Input/Denoise')
     parser.add_argument('--input_name', help = 'training image name', required = True)
     parser.add_argument('--denoise_scale', help='denoising scale', type=int, required=True)
     parser.add_argument('--mode', help='task to be done', default='harmonization')
-    parser.add_argument('--noise', help='sigma of Gaussian noise', default = 5.0)
+    parser.add_argument('--noise', help='sigma of Gaussian noise', default = 50)
     opt = parser.parse_args()
     opt = functions.post_config(opt)
     Gs = []
@@ -46,23 +47,50 @@ if __name__ == '__main__':
     if (opt.denoise_scale < 1) | (opt.denoise_scale > (len(Gs)-1)):
         print("injection scale should be between 1 and %d" % (len(Gs)-1))
     else:
-        clean_img = img.imread('%s/%s' % (opt.input_dir, opt.input_name)).astype(np.float32)
-        noisy_img = clean_img + np.random.normal(0, float(opt.noise), clean_img.shape)
-        noisy_img = functions.np2torch(noisy_img, opt)
-        noisy_img = noisy_img[:,0:3,:,:]
-        clean_img = clean_img[:,:,0:3] / 255.0
-
+        clean_img = img.imread('%s/%s' % (opt.input_dir, opt.input_name))[:,:,0:3]
+        #noisy_img, ref_img, ref_img_torch = noiser_and_ref(clean_img, opt)
+        #plt.figure()
+        #plt.imshow(noisy_img)
+        #plt.show()
+        #plt.figure()
+        #plt.imshow(ref_img)
+        #plt.show()
+        #noisy_img = clean_img + np.random.normal(0, float(opt.noise), clean_img.shape)
+        #noisy_img = functions.np2torch(noisy_img, opt)
+        #noisy_img = noisy_img[:,0:3,:,:]
+        #clean_img = clean_img[:,:,0:3] / 255.0
         N = len(reals) - 1
         n = opt.denoise_scale
-        noisy_img = functions.read_image_dir('denoised.png', opt)
-        in_s = imresize(noisy_img, pow(opt.scale_factor, (N - n + 1)), opt)
-        in_s = in_s[:, :, :reals[n - 1].shape[2], :reals[n - 1].shape[3]]
-        in_s = imresize(in_s, 1 / opt.scale_factor, opt)
-        in_s = in_s[:, :, :reals[n].shape[2], :reals[n].shape[3]]
-        out = SinGAN_denoise(Gs[n:], Zs[n:], reals, NoiseAmp[n:], opt, in_s, n=n, num_samples=1)
-        denoise_img = functions.convert_image_np(out.detach())
-        print(PSNR(denoise_img, clean_img))
-        #plt.imshow(clean_img)
-        plt.imshow(denoise_img)
-        plt.show()
-        plt.imsave('Output/denoise' + opt.input_name[:-4] + '_denoise' + opt.input_name[-4:], denoise_img, vmin =0.0, vmax = 1.0)
+
+        noisy_bm3d = []
+        noisy_img = img.imread('%s/%s/%s/noisy.png' % (opt.noise_dir, opt.input_name[:-4], str(opt.noise)))[:,:,0:3]
+        noisy_img_torch = functions.read_image_dir('%s/%s/%s/noisy.png' % (opt.noise_dir, opt.input_name[:-4], str(opt.noise)), opt)
+        noisy_bm3d.append(noisy_img_torch)
+        bm3d_img = img.imread('%s/%s/%s/denoised.png' % (opt.noise_dir, opt.input_name[:-4], str(opt.noise)))[:,:,0:3]
+        bm3d_img_torch = functions.read_image_dir('%s/%s/%s/denoised.png' % (opt.noise_dir, opt.input_name[:-4], str(opt.noise)), opt)
+        noisy_bm3d.append(bm3d_img_torch)
+        print('BM3D result:', compare_psnr(bm3d_img, clean_img))
+
+        for index, ref in enumerate(noisy_bm3d):
+            temp = functions.convert_image_np(ref) * 255
+            temp = temp.astype(np.uint8)
+            plt.figure()
+            plt.imshow(temp)
+            print(compare_psnr(temp, clean_img))
+            plt.show()
+            in_s = imresize(ref, pow(opt.scale_factor, (N - n + 1)), opt)
+            in_s = in_s[:, :, : reals[n - 1].shape[2], : reals[n - 1].shape[3]]
+            in_s = imresize(in_s, 1 / opt.scale_factor, opt)
+            in_s = in_s[:, :, : reals[n].shape[2], : reals[n].shape[3]]
+            out = SinGAN_denoise(Gs[n:], Zs[n:], reals, NoiseAmp[n:], opt, in_s, n=n, num_samples=1)
+            denoise_img = functions.convert_image_np(out.detach()) * 255
+            denoise_img = np.clip(denoise_img, 0, 255).astype(np.uint8)
+            plt.figure()
+            plt.imshow(denoise_img)
+            plt.show()
+            if index == 0:
+                name = 'No preprocessing'
+            else:
+                name = 'bm3d preprocessing'
+                plt.imsave('%s/%s/%s/SinGANdenoised.png' % (opt.noise_dir, opt.input_name[:-4], str(opt.noise)), denoise_img, vmin =0.0, vmax = 1.0)
+            print(name, ':', compare_psnr(denoise_img, clean_img))
